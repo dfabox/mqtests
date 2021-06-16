9d28f203d53c11edcbba34dcdb7674478f88cf54
d.fedyun <dfabox@hotmail.com>
2021-06-16 10:13:23 +0300

    Создание проекта.

---
+++ MQTests/Controllers/GeoInfoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MQTests.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GeoInfoController : ControllerBase
+    {
+        private readonly ILogger<GeoInfoController> _logger;
+
+        public GeoInfoController(ILogger<GeoInfoController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
